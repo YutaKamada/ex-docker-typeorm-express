19cf401ac2d1ede6b1831028e2d0fb3fb03bd3a7
YutaKamada <y.kamada1989@gmail.com>
2022-05-04 20:59:12 +0900

    動くように修正

--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -1,26 +1,29 @@
-import { getRepository } from "typeorm"
-import { NextFunction, Request, Response } from "express"
-import { User } from "../entity/User"
+import { getConnectionManager, getRepository } from 'typeorm';
+import { NextFunction, Request, Response } from 'express';
+import { User } from '../entity/User';
+import { AppDataSource } from '../data-source';
 
 export class UserController {
+  // NOTE: getRepository() は deprecated なため代替
+  // https://github.com/typeorm/typeorm/issues/7428#issuecomment-1072384216
+  private userRepository = AppDataSource.getRepository(User);
 
-    private userRepository = getRepository(User)
+  async all(request: Request, response: Response, next: NextFunction) {
+    return this.userRepository.find();
+  }
 
-    async all(request: Request, response: Response, next: NextFunction) {
-        return this.userRepository.find()
-    }
+  async one(request: Request, response: Response, next: NextFunction) {
+    return this.userRepository.findOne(request.params.id);
+  }
 
-    async one(request: Request, response: Response, next: NextFunction) {
-        return this.userRepository.findOne(request.params.id)
-    }
+  async save(request: Request, response: Response, next: NextFunction) {
+    return this.userRepository.save(request.body);
+  }
 
-    async save(request: Request, response: Response, next: NextFunction) {
-        return this.userRepository.save(request.body)
-    }
-
-    async remove(request: Request, response: Response, next: NextFunction) {
-        let userToRemove = await this.userRepository.findOneBy({ id: request.params.id })
-        await this.userRepository.remove(userToRemove)
-    }
-
+  async remove(request: Request, response: Response, next: NextFunction) {
+    let userToRemove = await this.userRepository.findOneBy({
+      id: request.params.id,
+    });
+    await this.userRepository.remove(userToRemove);
+  }
 }
