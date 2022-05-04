523e94e0618f604af8d3781ceb9c7b1ca839969f
YutaKamada <y.kamada1989@gmail.com>
2022-05-04 21:17:13 +0900

    セレクトを動くよう修正

--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -1,19 +1,24 @@
-import { getConnectionManager, getRepository } from 'typeorm';
 import { NextFunction, Request, Response } from 'express';
-import { User } from '../entity/User';
 import { AppDataSource } from '../data-source';
+import { User } from '../entity/User';
 
 export class UserController {
   // NOTE: getRepository() は deprecated なため代替
   // https://github.com/typeorm/typeorm/issues/7428#issuecomment-1072384216
   private userRepository = AppDataSource.getRepository(User);
+  private entitymanager = AppDataSource.manager;
 
   async all(request: Request, response: Response, next: NextFunction) {
     return this.userRepository.find();
   }
 
   async one(request: Request, response: Response, next: NextFunction) {
-    return this.userRepository.findOne(request.params.id);
+    // NOTE: entityManager / repository どちらでも実行可能
+    // return this.entitymanager
+    //   .createQueryBuilder(User, 'user')
+    //   .where('user.id = :id', { id: request.params.id })
+    //   .getOne();
+    return this.userRepository.findOneBy({ id: request.params.id });
   }
 
   async save(request: Request, response: Response, next: NextFunction) {
